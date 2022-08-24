8326b0e4fc8567d1848ea3d5ce4c4aac9d4a003e
Tailon Galvao <tailontuuc@gmail.com>
2022-08-24 01:24:59 -0300

    Criação de repositorio

--- src/main/java/br/com/springboot/curso_jdevtreinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdevtreinamento/controllers/GreetingsController.java
@@ -1,11 +1,15 @@
 package br.com.springboot.curso_jdevtreinamento.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.springboot.curso_jdevtreinamento.model.Usuario;
+import br.com.springboot.curso_jdevtreinamento.repository.UsuarioRepository;
 
 /**
  *
@@ -18,6 +22,14 @@ public class GreetingsController {
      * @param name the name to greet
      * @return greeting text
      */
+	
+	@Autowired //Injeão de dependencia
+	private UsuarioRepository usuarioRepository;
+	
+	
+	
+	
+	
     @RequestMapping(value = "/mostrarnome/{name}", method = RequestMethod.GET) //a URl onde sera passado o parametro
     @ResponseStatus(HttpStatus.OK)
     public String greetingText(@PathVariable String name) {
@@ -30,6 +42,9 @@ public class GreetingsController {
     @ResponseStatus(HttpStatus.OK)
     public String retornaOlaMundo(@PathVariable String name){
     	
+    	Usuario usuario = new Usuario();
+    	usuario.setNome(name);
+    	usuarioRepository.save(usuario);
     	return "Olá Mundo: " + name;
     }
     
